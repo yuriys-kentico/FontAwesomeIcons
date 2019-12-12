ec5fe63a6678bbc0fbc43f8b04b62055a94c15dd
Enngage <ennspassion@gmail.com>
2019-12-12 12:57:21 +0100

    initial font awesome plugin

--- src/app/element/FontAwesomeIcons.tsx
+++ src/app/element/FontAwesomeIcons.tsx
@@ -1,40 +1,104 @@
 import './FontAwesomeIcons.css';
 
-import React, { FC, useEffect, useRef, useState } from 'react';
-
 import { navigate } from '@reach/router';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { kenticoKontent } from '../../appSettings.json';
 import { loadModule } from '../../utilities/modules';
 import { RoutedFC } from '../../utilities/routing';
+import Icons from '../data/icons.json';
 import { Element, ICustomElement } from './customElement';
 
 // Expose access to Kentico custom element API
 declare const CustomElement: ICustomElement;
 
-interface IDetailsValue {}
-
-interface IDetailsConfig {}
-
 interface IFontAwesomeIconsProps {}
 
-const defaultDetailsValue: IDetailsValue = { customer: '', requester: '' };
+interface IFontAwesomeIconRaw {
+   svg: IFontAwesomeSvgWrapperRaw;
+   label: string;
+   search: {
+       terms: string[];
+   }
+}
+
+interface IFontAwesomeIcon {
+    svg: string;
+    title: string;
+    search: string;
+}
+
+interface IFontAwesomeSvgWrapperRaw {
+    brands?: IFontAwesomeSvgRaw;
+    solid?: IFontAwesomeSvgRaw;
+    regular?: IFontAwesomeSvgRaw;
+}
+
+interface IFontAwesomeSvgRaw {
+    raw: string;
+}
+
+const getDisplayedIcons: (search: string) => IFontAwesomeIcon[] = (search) => {
+    const icons: IFontAwesomeIcon[] = [];
+
+    for (const iconKey of Object.keys(Icons)) {
+        const icon = (Icons as any)[iconKey] as IFontAwesomeIconRaw;
+
+        let iconSvg: IFontAwesomeSvgRaw | undefined = undefined;
+
+        if (icon.svg.brands) {
+            iconSvg = icon.svg.brands;
+        } else if (icon.svg.regular) {
+            iconSvg = icon.svg.regular;
+        } else if (icon.svg.solid) {
+            iconSvg = icon.svg.solid;
+        }
+
+        if (iconSvg) {
+            icons.push({
+                title: icon.label,
+                svg: iconSvg.raw,
+                search: icon.search.terms.join(' ') + ' ' + icon.label
+            });
+        }
+
+    }
+
+    return icons.filter(m => m.search.toLowerCase().includes(search.toLowerCase() || '')).slice(0, kenticoKontent.iconsCount);
+}
 
 export const FontAwesomeIcons: RoutedFC<IFontAwesomeIconsProps> = () => {
   if (window.self === window.top) {
     navigate('/');
   }
 
-  const [ready, setReady] = useState(false);
+  const [, setReady] = useState(false);
   const [available, setAvailable] = useState(false);
+  const [selectedIconSvg, setSelectedIconSvg] = useState<string | undefined>(undefined);
   const [enabled, setEnabled] = useState(true);
+  const [, setSearchValue] = useState<string>('');
+  const [icons, setIcons] = useState<IFontAwesomeIcon[]>(getDisplayedIcons(''));
 
   const customElementRef = useRef<HTMLDivElement>(null);
-  const customElementConfig = useRef<IDetailsConfig>({});
+  const searchElementRef = useRef<HTMLInputElement>(null);
+
+  function handleSearch(search?: string): void {
+    setIcons(getDisplayedIcons(search || ''));
+    setSearchValue(search || '');
+  }
+
+  function selectIcon(icon: IFontAwesomeIcon): void {
+    CustomElement.setValue(icon.svg);
+    setSelectedIconSvg(icon.svg);
+  }
 
   useEffect(() => {
     const initCustomElement = (element: Element) => {
-      const elementValue = JSON.parse(element.value || JSON.stringify(defaultDetailsValue)) as IDetailsValue;
+      const elementValue = element.value;
+
+      if (elementValue) {
+        setSelectedIconSvg(elementValue);
+      }
 
       setAvailable(true);
 
@@ -59,41 +123,28 @@ export const FontAwesomeIcons: RoutedFC<IFontAwesomeIconsProps> = () => {
 
   useEffect(() => {
     if (available && customElementRef.current) {
-      CustomElement.setHeight(customElementRef.current.scrollHeight);
+      CustomElement.setHeight(customElementRef.current.scrollHeight + 30);
     }
   });
 
-  useEffect(() => {
-    if (available) {
-      CustomElement.setValue(JSON.stringify({}));
-    }
-  }, [available]);
-
   return (
     <div className={`custom element ${enabled ? '' : 'disabled'}`} ref={customElementRef}>
+        {selectedIconSvg ? (
+            <div className="selected-icon-wrapper">
+            <div className="selected-icon">Selected icon: </div>
+            <div dangerouslySetInnerHTML={{ __html: selectedIconSvg }}></div>
+            </div>) : <div>No icon is selected</div>}
       {available && (
-        <>
-          <div className='text element'>
-            <div className='pane'>
-              <label className='label'>header</label>
-              <div className='guidelines'>
-                <p>guidelines</p>
-              </div>
+        <div>
+        {enabled && (<>
+          <input ref={searchElementRef} className="search-input" placeholder="Search icon" type="text" onChange={(m) => handleSearch(searchElementRef.current ? searchElementRef.current.value : '')}></input>
+            <div className="icons-wrapper">
             </div>
-          </div>
-          {!enabled && (
-            <>
-              {ready && (
-                <div className='element'>
-                  <div className='pane'>
-                    <label className='label'>header</label>
-                    explanation
-                  </div>
-                </div>
-              )}
-            </>
-          )}
-        </>
+            {icons.map(icon => <div title={icon.title} className="svg-wrapper" onClick={(e) => selectIcon(icon)}
+                dangerouslySetInnerHTML={{ __html: icon.svg }}>
+            </div>)}
+        </>)}
+        </div>
       )}
     </div>
   );
